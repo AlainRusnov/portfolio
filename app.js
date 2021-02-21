c24f95db338ea7f0393dc2a9c588e5843a0d26fe
Alain Rusnov <alainrusnov@gmail.com>
2021-02-21 01:33:43 -0500

    initial proto fenestration grid, extra layers

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function init() {
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth * 1, window.innerHeight * 1);
-  // renderer.setClearColor(0x00ffff, 1);
+  renderer.setClearColor(0x0000FF, 0.1);
   document.body.appendChild(renderer.domElement);
 
   // Camera/Mouse controls - Issues on file restructure for three.js error nil[i]
@@ -56,13 +56,13 @@ function init() {
     // Cube 1 texture map
   let materialArray1 = [];
   let texture_ft = new THREE.TextureLoader().load('./img/left.png');
-  let texture_bk = new THREE.TextureLoader().load('./img/left.png');
-  let texture_up = new THREE.TextureLoader().load('./img/up.jpg');
+  let texture_bk = new THREE.TextureLoader().load('./img/door.png');
+  let texture_up = new THREE.TextureLoader().load('./img/up1.png');
   let texture_dn = new THREE.TextureLoader().load('./img/down.png');
   let texture_rt = new THREE.TextureLoader().load('./img/right.png');
   let texture_lf = new THREE.TextureLoader().load('./img/Hero.png');
 
-  materialArray1.push(new THREE.MeshBasicMaterial({ opacity: 0, transparent: true, map: texture_ft }));
+  materialArray1.push(new THREE.MeshBasicMaterial({ opacity: 0, transparent: true }));
   materialArray1.push(new THREE.MeshBasicMaterial({ map: texture_bk }));
   materialArray1.push(new THREE.MeshBasicMaterial({ map: texture_up }));
   materialArray1.push(new THREE.MeshBasicMaterial({ map: texture_dn }));
@@ -77,8 +77,8 @@ function init() {
   // Cube 2 texture map
   let materialArray2 = [];
   let texture_ft2 = new THREE.TextureLoader().load('./img/right.png');
-  let texture_bk2 = new THREE.TextureLoader().load('./img/right.png');
-  let texture_up2 = new THREE.TextureLoader().load('./img/up.jpg');
+  let texture_bk2 = new THREE.TextureLoader().load('./img/window.png');
+  let texture_up2 = new THREE.TextureLoader().load('./img/up2.png');
   let texture_dn2= new THREE.TextureLoader().load('./img/down.png');
   let texture_rt2 = new THREE.TextureLoader().load('./img/dawg.png');
   let texture_lf2 = new THREE.TextureLoader().load('./img/right.png');
@@ -116,7 +116,7 @@ function init() {
   let materialArray4 = [];
   let texture_ft4 = new THREE.TextureLoader().load('./img/gblogo.png');
   let texture_bk4 = new THREE.TextureLoader().load('./img/right.png');
-  let texture_up4 = new THREE.TextureLoader().load('./img/up.jpg');
+  let texture_up4 = new THREE.TextureLoader().load('./img/up4.png');
   let texture_dn4= new THREE.TextureLoader().load('./img/down.png');
   let texture_rt4 = new THREE.TextureLoader().load('./img/Gumwall.png');
   let texture_lf4 = new THREE.TextureLoader().load('./img/stuud.png');
