37b3102c6f50f3dce25e876b2d8f8d434e1d7867
Alain Rusnov <alainrusnov@gmail.com>
2021-03-06 15:56:40 -0500

    tested blocks - set static

--- flat.js
+++ flat.js
@@ -75,23 +75,35 @@ let groups = [];
 let attractMode = false;
 let attractTo = 0;
 let camera, scene, renderer;
-let geometry, mat, mesh;
+let geoback, mat, mesh;
 let bkground = [0xff, 0xffff, 0x000, 0xffffff, 0x000ff];
+
+var W, H;
+  // var camera, scene, renderer;
+  var geoRec, matBox
+  var pointLight;
+  var obj = {};
+  var mouseX = 0;
+  var mouseY = 0;
+  var nbmesh = 100;
 
 init();
 
 function init() {
 
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10000 );
   camera.position.z = 1;
+
+  W = window.innerWidth;
+  H = window.innerHeight;
 
   scene = new THREE.Scene();
   // scene.background = new THREE.color( 0xff0000 );
 
-  // geometry = new THREE.PlaneBufferGeometry( 1,1 );
+  // geoRec = new THREE.PlaneBufferGeometry( 1,1 );
   // material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
 
-  // mesh = new THREE.Mesh( geometry, material );
+  // mesh = new THREE.Mesh( geoRec, material );
   // scene.add( mesh );
 
   renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
@@ -99,6 +111,34 @@ function init() {
   renderer.setClearColor(0xFFFFFF, 0);
   // renderer.setAnimationLoop( animation );
   container.appendChild( renderer.domElement );
+
+   // LIGHTS
+   pointLight = new THREE.PointLight(0xFFFFFF, 1.0, 1500);
+   pointLight.position.x = 0;
+   pointLight.position.y = 0;
+   pointLight.position.z = 1500;
+   scene.add(pointLight);
+
+  //  geoRec = new THREE.BoxBufferGeometry( 1000, 500, 500 );
+
+
+  //   var box;
+  //   var color;
+  //   for (var i = 0; i < nbmesh; i++) {
+  //     color = new THREE.Color( 0xffffff );
+  //     color.setRGB( 255, 255, 255 );
+
+  //     matBox = new THREE.MeshPhongMaterial( { color:color, wireframe: false } );
+
+  //     obj['box'+i] = new THREE.Mesh( geoRec, matBox );
+
+  //     scene.add( obj['box'+i] );
+  //     box = obj['box' + i];
+
+  //     box.position.x = (( -W* 2) * Math.random()) + W;
+  //     box.position.y = (( -H* 4) * Math.random()) + H;
+  //     box.position.z = (( -H *4) * Math.random()) + H;
+  //   }
 
   animate();
   handleImages();
@@ -127,10 +167,12 @@ function handleImages(){
     mesh.position.y = i*1.2;
     mesh.position.x = 0.3;
     // console.log(mesh);
+    // mesh.position.z = 1500;
 
     group.rotation.y = -0.3;
     group.rotation.x = -0.2;
     group.rotation.z = -0.2;
+
 
     // renderer.setClearColor(0xffffff, 0);
     // console.log(bkground[i]);
@@ -202,6 +244,19 @@ function animate() {
   }
 	// mesh.rotation.x = time / 2000;
 	// mesh.rotation.y = time / 1000;
+
+  // for ( var i = 0; i < nbmesh; i++ ) {
+  //   var box = obj['box'+i];
+  //   box.rotation.x += ((W * .5) ) * .0000005;
+  //   box.rotation.y += ((H * .5) - mouseX) * .0000005;
+  //   box.position.z  = ((H * .5) - mouseY) * 1.0;
+  // }
+
+  pointLight.distance = (mouseY * 2) +2000;
+  // camera.position.y = (mouseY * 0.5);
+  // camera.position.x = (mouseX * 0.5);
+
+
   renderer.render(scene, camera);
   // controls.update();
   requestAnimationFrame(animate);
@@ -242,3 +297,100 @@ navs.forEach(el=>{
 
 
 
+// function Background(){
+//   var self = this;
+//   var W, H;
+//   var camera, scene, renderer;
+//   var geoback, matBox
+//   var pointLight;
+//   var obj = {};
+//   var mouseX = 0;
+//   var mouseY = 0;
+//   var nbmesh = 100;
+
+
+//   this.init = function(){
+//     // SET
+//     scene = new THREE.Scene();
+//     W = window.innerWidth
+//     H = window.innerHeight
+
+    // LIGHTS
+    // pointLight = new THREE.PointLight(0xFFFFFF, 1.5, 1000);
+	  // pointLight.position.x = 0;
+		// pointLight.position.y = 0;
+		// pointLight.position.z = 100;
+		// scene.add(pointLight);
+
+    // CAMERA
+    // camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    // camera.position.z = 1000;
+
+    // CAST
+    // geoback = new THREE.BoxGeometry( 600, 200, 200 );
+
+
+    // var box;
+    // var color;
+    // for (var i = 0; i < nbmesh; i++) {
+    //   color = new THREE.Color( 0xffffff );
+    //   color.setRGB( 255, 255, 255 );
+
+    //   matBox = new THREE.MeshPhongMaterial( { color:color, wireframe: false } );
+
+    //   obj['box'+i] = new THREE.Mesh( geoback, matBox );
+
+    //   scene.add( obj['box'+i] );
+    //   box = obj['box' + i];
+
+    //   box.position.x = ((- W* 2) * Math.random()) + W;
+    //   box.position.y = ((- H* 4) * Math.random()) + H;
+    //   box.position.z = ((- H *4) * Math.random()) + H;
+    // }
+
+    // ACTION
+    // renderer = new THREE.WebGLRenderer();
+    // renderer.setSize( window.innerWidth, window.innerHeight );
+    // document.getElementById('container').appendChild( renderer.domElement );
+
+  //   window.addEventListener( "resize", this.onresize)
+  //   document.addEventListener( "mousemove", this.onmousemove)
+  // }
+
+//   this.animate = function(){
+//     requestAnimationFrame( self.animate );
+
+
+//     for ( var i = 0; i < nbmesh; i++ ) {
+//       var box = obj['box'+i];
+//       box.rotation.x += ((W * .5) ) * .0000005;
+//       box.rotation.y += ((H * .5) - mouseX) * .0000005;
+//       box.position.z  = ((H * .5) - mouseY) * 1.0;
+//     }
+
+//     pointLight.distance = (mouseY * 2) +1000;
+//     camera.position.y = (mouseY * 0.5);
+//     camera.position.x = (mouseX * 0.5);
+
+//     renderer.render( scene, camera );
+//   }
+
+//   this.onresize = function(){
+//     W = window.innerWidth
+//     H = window.innerHeight
+//     camera.aspect = W / H;
+//     camera.updateProjectionMatrix();
+//     renderer.setSize(W, H, true);
+//     renderer.render(scene, camera);
+//   }
+
+//   this.onmousemove = function(e){
+//     mouseX = e.pageX;
+//     mouseY = e.pageY;
+//   }
+
+//   this.init();
+// 	this.animate();
+// }
+
+// new Background();
